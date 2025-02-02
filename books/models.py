d4a1fc27bd283cb1b627f26eac965bf415e480a3
kostrv <gaponov.nik.07@gmail.com>
2025-02-02 18:12:00 +0500

    refactor models: clean up whitespace in Author and Book models

--- books/models.py
+++ books/models.py
@@ -3,17 +3,17 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
     img_src = models.URLField(blank=True, null=True)
-    years_of_age = models.CharField(max_length=50)  # Если не только числа
+    years_of_age = models.CharField(max_length=50)  
     bio = models.TextField()
-    works = models.TextField(blank=True, null=True)  # Перечень книг через запятую
-
+    works = models.TextField(blank=True, null=True) 
+    
     def __str__(self):
         return self.name
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  
     img_src = models.URLField(blank=True, null=True)
     publication_date = models.DateField()
     short_desc = models.TextField()
